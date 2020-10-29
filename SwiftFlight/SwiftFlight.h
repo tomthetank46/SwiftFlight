4d16b08ceff1b0c137ed5f08d0141d3f1347bfb6
Thomas Hogrefe <tomhogrefe46@gmail.com>
2020-10-29 14:53:37 -0400

    Initial Commit

---
+++ SwiftFlight/SwiftFlight.h
@@ -0,0 +1,18 @@
+//
+//  SwiftFlight.h
+//  SwiftFlight
+//
+//  Created by Thomas Hogrefe on 10/29/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftFlight.
+FOUNDATION_EXPORT double SwiftFlightVersionNumber;
+
+//! Project version string for SwiftFlight.
+FOUNDATION_EXPORT const unsigned char SwiftFlightVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftFlight/PublicHeader.h>
+
+
